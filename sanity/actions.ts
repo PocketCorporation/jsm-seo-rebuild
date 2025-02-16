ee0e69aef44ce7a653284a133ab659988d3fdb20
pocketcorp <pocketcorp@protonmail.com>
2025-02-15 23:44:02 -0800

    added sanity env token

---
+++ sanity/actions.ts
@@ -0,0 +1,16 @@
+import { groq } from "next-sanity";
+
+interface GetResourcesParams{
+    query:string;
+    category:string;
+    page:string;
+}
+
+export const getResources = async (params: GetResourcesParams)=>{
+    const{query,category,page}=params
+    try{
+        
+    } catch(error) {
+
+    }
+}
